bd39fcbff736a5a2b79ce4d242898c973c24541d
Yaroslav Fedorov <Fedorov.mail@gmail.com>
2012-01-29 12:35:19 +0400

    Initial Commit

---
+++ test_app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test_app
+//
+//  Created by Yaroslav Fedorov on 29/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test_app/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test_app
+//
+//  Created by Yaroslav Fedorov on 29/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
