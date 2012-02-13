d687ef94a05bb2ff230f9ee36b3bd23663668909
Yaroslav Fedorov <Fedorov.mail@gmail.com>
2012-02-13 04:35:34 +0400

    Final test_app commit

---
+++ test_app/SelCorrectAnswerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SelCorrectAnswerTableViewController.h
+//  test_app
+//
+//  Created by Yaroslav Fedorov on 12/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelCorrectAnswerTableViewController : UITableViewController
+
+@end
